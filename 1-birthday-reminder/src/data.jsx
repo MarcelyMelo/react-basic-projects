c3df0b177d68093fbf92e236b963cae5e59f9f04
Marcely Melo <marcelyjfmelo@gmail.com>
2022-06-29 20:44:16 -0300

    birthday reminder project

---
+++ 1-birthday-reminder/src/data.jsx
@@ -0,0 +1,32 @@
+export default[
+    {
+        id:1,
+        name: 'Marcy',
+        age: 17,
+        image: 'https://github.com/marcelymelo.png?size=460'
+    },
+    {
+        id:2,
+        name: 'Adriel',
+        age: 19,
+        image: 'https://github.com/adrielestoller.png?size=460'
+    },
+    {
+        id:3,
+        name: 'Victor',
+        age: 19,
+        image: 'https://github.com/LordDarkn3ss.png?size=460'
+    },
+    {
+        id:4,
+        name: 'Marvin',
+        age: 18,
+        image: 'https://github.com/MisterMarv.png?size=460'
+    },
+    {
+        id:5,
+        name: 'David',
+        age: 18,
+        image: 'https://github.com/D-Felipe.png?size=460'
+    }
+]
